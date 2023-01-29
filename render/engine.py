bb3931f85ac2d7cd72161e4161c413a26a6391aa
joeyskeys <joey.skeys@gmail.com>
2023-01-29 21:01:38 +0800

    add render thread and some sample in comment

--- render/engine.py
+++ render/engine.py
@@ -1,9 +1,22 @@
 
 import os
 import bpy
+import threading
 import time
 from .. import config
 from ..utils.registry import regular_registry
+
+
+class BittoRenderThread(threading.Thread):
+    def __init__(self, renderer):
+        super(BittoRenderThread, self).__init__()
+        self.renderer = renderer
+
+    def run(self):
+        # Something like the following line to actually start
+        # the render
+        # self.renderer.render()
+        pass
 
 
 class BittoRenderEngine(bpy.types.RenderEngine):
@@ -16,12 +29,18 @@ class BittoRenderEngine(bpy.types.RenderEngine):
     # Ctor
     def __init__(self):
         self.renderer = None
+        self.render_thread = None
 
     def __del__(self):
         pass
 
     def render(self, depsgraph):
         print("Currently just print 'rendering'")
+        # Setup rendering thread to keep blender responsive
+        # self.render_thread = BittoRenderThread(self.renderer)
+        # self.render_thread.start()
+        # while self.render_thread.isAlive()
+        #     self.render_thread.join(0.5)
 
     def view_update(self, context, depsgraph):
         pass
