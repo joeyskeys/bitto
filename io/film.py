1c65ad0f68109cc164b4d027f74b1ea71ab5d0de
joeyskeys <joey.skeys@gmail.com>
2023-09-04 14:56:43 +0800

    remove unrelated modification

--- io/film.py
+++ io/film.py
@@ -15,6 +15,10 @@ class FilmIO(BaseIO):
         render = bpy.context.scene.render
         return (render.resolution_x, render.resolution_y)
 
+    def get_ratio(self):
+        render = bpy.context.scene.render
+        return render.resolution_x / render.resolution_y
+
     def get_props(self):
         return bpy.context.scene.bitto_film_props
 
